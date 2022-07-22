a1402c923c289b1bc11a54b911f75b3f17e2778e
Nicole <nicole.molina.pb@compasso.com.br>
2022-07-22 12:04:23 -0300

    Criação do tema

---
+++ lib/core/theme/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kDefaultPadding = EdgeInsets.symmetric(horizontal: 20, vertical: 25);
+const kHorizontalPadding = EdgeInsets.symmetric(horizontal: 20);
+const kVerticalPadding = EdgeInsets.symmetric(vertical: 25);
