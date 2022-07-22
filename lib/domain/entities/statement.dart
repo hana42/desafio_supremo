6012a8d51c66857fc98a55afb8e86a0c27e22330
Nicole <nicole.molina.pb@compasso.com.br>
2022-07-22 12:33:28 -0300

    add entities

---
+++ lib/domain/entities/statement.dart
@@ -0,0 +1,81 @@
+import 'package:equatable/equatable.dart';
+
+class Statement extends Equatable {
+  const Statement({
+    required this.createdAt,
+    required this.id,
+    required this.amount,
+    this.to,
+    required this.description,
+    required this.tType,
+    this.from,
+    this.bankName,
+  });
+
+  final DateTime createdAt;
+  final String id;
+  final int amount;
+  final String? to;
+  final String description;
+  final String tType;
+  final String? from;
+  final String? bankName;
+
+  Statement copyWith({
+    required DateTime createdAt,
+    required String id,
+    required int amount,
+    String? to,
+    required String description,
+    required String tType,
+    String? from,
+    String? bankName,
+  }) =>
+      Statement(
+        createdAt: createdAt,
+        id: id,
+        amount: amount,
+        to: to,
+        description: description,
+        tType: tType,
+        from: from,
+        bankName: bankName,
+      );
+
+  factory Statement.fromJson(Map<String, dynamic> json) => Statement(
+        createdAt: DateTime.parse(json['createdAt']),
+        id: json['id'],
+        amount: json['amount'],
+        to: json['to'],
+        description: json['description'],
+        tType: json['tType'],
+        from: json['from'],
+        bankName: json['bankName'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'createdAt': createdAt.toIso8601String(),
+        'id': id,
+        'amount': amount,
+        'to': to,
+        'description': description,
+        'tType': tType,
+        'from': from,
+        'bankName': bankName,
+      };
+
+  Statement toEntity() => Statement(
+        createdAt: createdAt,
+        id: id,
+        amount: amount,
+        to: to,
+        description: description,
+        tType: tType,
+        from: from,
+        bankName: bankName,
+      );
+
+  @override
+  List<Object?> get props =>
+      [createdAt, id, amount, to, description, tType, from, bankName];
+}
