87d4c5543dc0c30fc070e2ce682d6552ec69033d
Nicole <nicole.molina.pb@compasso.com.br>
2022-08-12 00:13:55 -0300

    add savings screen

--- lib/presentation/pages/savings/savings_screen.dart
+++ lib/presentation/pages/savings/savings_screen.dart
@@ -5,14 +5,41 @@ import 'package:desafio_supremo/presentation/pages/savings/components/goal_list.
 import 'package:desafio_supremo/presentation/pages/savings/components/goal_form_modal.dart';
 import 'package:desafio_supremo/presentation/widgets/balance/balance_widget.dart';
 
-class SavingsScreen extends StatelessWidget {
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:desafio_supremo/core/theme/constants.dart';
+import 'package:desafio_supremo/injection.dart';
+import 'package:desafio_supremo/presentation/bloc/goals/goals_cubit.dart';
+import 'package:desafio_supremo/presentation/widgets/user_goals_item_card.dart';
+
+class SavingsScreen extends StatefulWidget {
   const SavingsScreen({Key? key}) : super(key: key);
 
   @override
+  State<SavingsScreen> createState() => _SavingsScreenState();
+}
+
+class _SavingsScreenState extends State<SavingsScreen> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _goalController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _goalController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Poupança'),
         centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
